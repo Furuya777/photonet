42e1269cf948f920a7d10e41d9e5d63b7fdc1c82
Sonoko Furuya <sfuruya@r-gene.co.jp>
2014-04-25 16:49:33 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photonet::Application.config.secret_key_base = 'd3c96f19b60d9fc47c1f96cf2c42f07a56b53e04287a940bdcfaddee3d8fb36f51faf464f9d3202b226467abc895699537129b7049fc2784ac7996a8f4c428a5'
