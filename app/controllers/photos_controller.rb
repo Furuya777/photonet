adffa4b9fd47528bf56d76b583fa87ef8054d094
Sonoko Furuya <sfuruya@r-gene.co.jp>
2014-05-09 22:14:51 +0900

    kaminariのthemeをuserとadminで分けた

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,15 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show]
 
+  RESULT_PER_PAGE = 6
+
   def index
     if params[:tag].present?
-      @photos = Photo.tag(params[:tag]).order_created_at
+      photos = Photo.tag(params[:tag]).order_created_at
     else
-      @photos = Photo.all.order_created_at
+      photos = Photo.all.order_created_at
     end
+    @photos = photos.page(params[:page]).per(RESULT_PER_PAGE)
   end
 
   def show
